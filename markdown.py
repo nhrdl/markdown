5e55ee4ffc519679863c94f19d8fc49eb717d9a6
niranjan <niranjan@fiport.com>
2013-03-01 13:45:16 -0800

    Converted the script code to object

--- markdown.py
+++ markdown.py
@@ -5,37 +5,40 @@ import glib
 import webkit 
 import os;
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="Markdown file")
-args = parser.parse_args()
-print "File ", os.path.abspath(args.file)
 
-idle_index = ""
+class App:
+    
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("file", help="Markdown file")
+        args = parser.parse_args()
 
-toolbar = gtk.Toolbar()
-toolbar.set_style(gtk.TOOLBAR_ICONS)
-savetb = gtk.ToolButton(gtk.STOCK_SAVE)
-sep = gtk.SeparatorToolItem()
-quittb = gtk.ToolButton(gtk.STOCK_QUIT)
-toolbar.insert(savetb, 0)
-toolbar.insert(sep, 1)
-toolbar.insert(quittb, 2)
+
+        toolbar = gtk.Toolbar()
+        toolbar.set_style(gtk.TOOLBAR_ICONS)
+        savetb = gtk.ToolButton(gtk.STOCK_SAVE)
+        sep = gtk.SeparatorToolItem()
+        quittb = gtk.ToolButton(gtk.STOCK_QUIT)
+        toolbar.insert(savetb, 0)
+        toolbar.insert(sep, 1)
+        toolbar.insert(quittb, 2)
         
-view = webkit.WebView()
-sw = gtk.ScrolledWindow() 
-sw.add(view) 
+        self.view = webkit.WebView()
+        sw = gtk.ScrolledWindow() 
+        sw.add(self.view) 
 
-win = gtk.Window(gtk.WINDOW_TOPLEVEL)
-vbox = gtk.VBox()
-vbox.pack_start(toolbar, False, False, 0)
-win.add(vbox)
-vbox.add(sw)
-#sw.set_size_request(900, 900)
+        win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        vbox = gtk.VBox()
+        vbox.pack_start(toolbar, False, False, 0)
+        win.add(vbox)
+        vbox.add(sw)
 
-win.show_all() 
-win.connect("delete-event", gtk.main_quit)
-view.open("file://" + os.path.abspath(args.file))
-win.maximize()
+        win.show_all() 
+        win.connect("delete-event", gtk.main_quit)
+        self.view.open("file://" + os.path.abspath(args.file))
+        win.maximize()
 #idle_index=glib.idle_add(idleHookFunction)
+
+App()        
 gtk.main()
 
